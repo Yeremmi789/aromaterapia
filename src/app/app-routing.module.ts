6d3347ece7e356e388f42593925f396d2086f44b
Yeremmi789 <yeremmi789@gmail.com>
2023-05-30 15:44:21 -0600

    catalogo v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GeneralComponent } from './modulos/pages/productos/general/general.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: GeneralComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneralComponent } from './modulos/pages/productos/general/general.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralComponent
   ],
   imports: [
     BrowserModule,
