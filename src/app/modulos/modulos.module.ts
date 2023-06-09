0766c5833c43c023c32df62baa5d505731eee89b
05JesusMoreno <jesusbautistam845@gmail.com>
2023-06-09 15:24:52 -0600

    Implementacion de modulo de Preguntas Frecuentes

--- src/app/modulos/modulos.module.ts
+++ src/app/modulos/modulos.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetallesComponent } from './pages/productos/detalles/detalles.component';
 import { GeneralComponent } from './pages/productos/general/general.component';
+import { PreguntasFrecuentesComponent } from './pages/productos/general/preguntas-frecuentes/preguntas-frecuentes.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 
 
-
 @NgModule({
   declarations: [
     DetallesComponent,
-    GeneralComponent
+    GeneralComponent,
+    PreguntasFrecuentesComponent,
+    
   ],
   imports: [
     CommonModule,
