d5be7d0518898570f5d3cb070289c8d164f26fdc
Yeremmi789 <yeremmi789@gmail.com>
2023-06-16 23:20:47 -0600

    Implementación del modulo Home y Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { GeneralComponent } from './modulos/pages/productos/general/general.component';
 import { DetallesComponent } from './modulos/pages/productos/detalles/detalles.component';
 import { HomeComponent } from './modulos/home/home.component';
+import { LoginComponent } from './modulos/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: GeneralComponent,
+    component: HomeComponent,
     pathMatch: 'full',
   },
   {
     path: 'productos',
     component: DetallesComponent,
   },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'catalogo',
+    component: GeneralComponent
+  }
 ];
 
 @NgModule({
